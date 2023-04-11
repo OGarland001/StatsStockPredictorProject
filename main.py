2b640251ba3bb6d63e8d482b46281dc68e88f642
Owen Garland <Ogarland9238@conestogac.on.ca>
2023-04-11 16:36:23 -0400

    model working? for google

--- main.py
+++ main.py
@@ -20,9 +20,119 @@ from tensorflow.random import set_seed
 from pandas_datareader.data import DataReader
 
 import warnings
-#warnings.simplefilter("ignore")
+warnings.simplefilter("ignore")
 
-#set_seed(455)
-#np.random.seed(455)
+set_seed(455)
+np.random.seed(455)
 
-print("Im drowing pleease help me")
+end = datetime.now()
+start = datetime(2016, end.month, end.day)
+dataset = yf.download("GOOGL", start, end)
+
+tstart = 2010
+tend = 2022
+
+def train_test_plot(dataset, tstart, tend):
+    dataset.loc[f"{tstart}":f"{tend}", "High"].plot(figsize=(16, 4), legend=True)
+    dataset.loc[f"{tend+1}":, "High"].plot(figsize=(16, 4), legend=True)
+    plt.legend([f"Train (Before {tend+1})", f"Test ({tend+1} and beyond)"])
+    plt.title("GOOGL stock price")
+    plt.show()
+
+train_test_plot(dataset,tstart,tend)
+
+
+def train_test_split(dataset, tstart, tend):
+    train = dataset.loc[f"{tstart}":f"{tend}", "High"]
+    test = dataset.loc[f"{tend+1}":, "High"]
+    return train, test
+
+def train_test_split_values(dataset, tstart, tend):
+    train, test =  train_test_split(dataset, tstart, tend)
+    return train.values, test.values
+
+training_set, test_set = train_test_split_values(dataset, tstart, tend)
+
+# Scaling the training set
+sc = MinMaxScaler(feature_range=(0, 1))
+training_set = training_set.reshape(-1, 1)
+training_set_scaled = sc.fit_transform(training_set)
+
+def split_sequence(sequence, window):
+    X, y = list(), list()
+    for i in range(len(sequence)):
+        end_ix = i + window
+        if end_ix > len(sequence) - 1:
+            break
+        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
+
+window_size = 60
+features = 1
+
+X_train, y_train = split_sequence(training_set_scaled, window_size)
+
+X_train = X_train.reshape(X_train.shape[0],X_train.shape[1],features)
+
+model_lstm = Sequential()
+model_lstm.add(LSTM(units=125, activation="tanh", input_shape=(window_size, features)))
+model_lstm.add(Dense(25))
+model_lstm.add(Dense(units=1))
+
+model_lstm.compile(optimizer='adam', loss='mse')
+
+model_lstm.summary()
+
+
+model_lstm.fit(X_train, y_train, epochs=15, batch_size=32)
+
+dataset_total = dataset.loc[:,"High"]
+inputs = dataset_total[len(dataset_total) - len(test_set) - window_size :].values
+inputs = inputs.reshape(-1, 1)
+inputs = sc.transform(inputs)
+
+X_test, y_test = split_sequence(inputs, window_size)
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], features)
+predicted_stock_price = model_lstm.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+y_test = sc.inverse_transform(y_test)
+
+
+def plot_predictions(test, predicted):
+    plt.plot(test, color="gray", label="Real")
+    plt.plot(predicted, color="red", label="Predicted")
+    plt.title("Stock Price Prediction")
+    plt.xlabel("Time")
+    plt.ylabel("Stock Price")
+    plt.legend()
+    plt.show()
+
+
+def return_rmse(test, predicted):
+    rmse = np.sqrt(mean_squared_error(test, predicted))
+    print("The root mean squared error is {:.2f}.".format(rmse))
+
+    
+plot_predictions(y_test,predicted_stock_price)
+
+return_rmse(y_test,predicted_stock_price)
+
+model_gru = Sequential()
+model_gru.add(GRU(units=125, activation="tanh", input_shape=(window_size, features)))
+model_gru.add(Dense(units=1))
+
+model_gru.compile(optimizer="adam", loss="mse")
+
+model_gru.summary()
+
+model_gru.fit(X_train, y_train, epochs=10, batch_size=32)
+
+GRU_predicted_stock_price = model_gru.predict(X_test)
+GRU_predicted_stock_price = sc.inverse_transform(GRU_predicted_stock_price)
+
+plot_predictions(y_test, GRU_predicted_stock_price)
+
+
+return_rmse(y_test,GRU_predicted_stock_price)
