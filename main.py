a891f71a443c1a84bd9cbab441d4e901ded2ac7a
Owen Garland <Ogarland9238@conestogac.on.ca>
2023-04-11 16:01:04 -0400

    imported librarys

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Brodin likes wenor")
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime
+import yfinance as yf
+import tensorflow as tf
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, GRU
+from tensorflow.keras.optimizers import SGD
+from tensorflow.random import set_seed
+
+from pandas_datareader.data import DataReader
+
+import warnings
+#warnings.simplefilter("ignore")
+
+#set_seed(455)
+#np.random.seed(455)
+
+print("Im drowing pleease help me")
